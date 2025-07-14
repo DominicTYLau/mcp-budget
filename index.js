7affbc508d042daf50906a1386fec4bf62cdf1bc
DominicTYLau <dlau1480@gmail.com>
2025-07-13 21:48:14 -0700

    update

--- index.js
+++ index.js
@@ -5,20 +5,15 @@ import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js"
 import { z } from "zod";
 import fs from 'fs';
 
-
-// Create the MCP server
 const server = new McpServer({
   name: "mcp-budget-server",
   version: "1.0.0"
 });
 
-// Input schema using plain JSON Schema
 const inputSchema = {
 }
 
-// The function to fetch news
 async function fetchBudget({question}) {
-    // Load the CSV file
         let csvData ;
 
     try {
@@ -35,13 +30,11 @@ async function fetchBudget({question}) {
   };
 }
 
-// Register the tool
 server.registerTool("budget", {
   title: "Budget Search",
   description: "Searches a persons budget and provides details on it.",
   inputSchema
 }, fetchBudget);
 
-// Start the MCP server
 const transport = new StdioServerTransport();
 await server.connect(transport);
